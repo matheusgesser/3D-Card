29460f65fb1d3eb0cc0a49dc62490cccf8803901
“Matheus <matheusmvg@hotmail.com>
2023-01-14 18:58:36 -0300

    fix

--- script.js
+++ script.js
@@ -12,25 +12,21 @@ const container = document.querySelector('.container');
 const sneaker = document.querySelector(".sneaker");
 const description = document.querySelector(".info");
 
-//Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-    console.log(e.pageX, e.pageY);
     let xAxis = (window.innerWidth / 2 - e.pageX) / 35;
     let yAxis = (window.innerHeight / 2 - e.pageY) / 35;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
-//Animate In
+
 container.addEventListener("mouseenter", (e) => {
-    //Popout
     sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
     description.style.transform = "translateZ(150px)";
     card.style.transition = "none";
 });
-//Animate Out
+
 container.addEventListener("mouseleave", (e) => {
     card.style.transition = "all 0.5s ease-in-out";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    //Popback
     sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
     description.style.transform = "translateZ(0px)";
 });
